d899dc058698371dab36819fce5e0eb7dd7b6c4f
mohammed faizan <faizan1999.in@gmail.com>
2025-08-03 21:29:39 +0530

    fixed logo error in navbar

--- src/Landing/Navbar.jsx
+++ src/Landing/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className=" top-0 navbar bg-base-200 px-6">
       <div className="flex items-center">
-        <img src="/vite.svg" alt="Logo" style={{ width: 64, height: 64 }} />
+        <img src="/logo.png" alt="Logo" style={{ width: 64, height: 64 }} />
         <p className="font-medium text-xl">Talko</p>
       </div>
 
